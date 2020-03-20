31c18fc891afb4149602e1fc221731393d0552ae
Leo-793 <tognonleonardo@gmail.com>
2020-03-20 11:27:30 +0100

    Update index.js

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -37,7 +37,7 @@ export default class LandingPage extends Component {
 					<div className={`login-panel ${userData ? "logged" : ""}`}>
 						<div>
 							<div className="my-form intro">
-								<span>COMMERCIALE4.0.COM</span> is the community of
+								<span>COMMERCIALE4.0.COM</span> is THE community of
 								mechanical companies. We
 								revolutionize the way companies connect with
 								each other, building the networking of the
